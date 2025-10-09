abfae1c6237c6e434b776f7f0071385d57ead5a1
Daniel D’Aquino <daniel@daquino.me>
2025-10-09 11:23:29 -0700

    Update template to use Lumen Blocks 0.2.0 and Dioxus 0.7 RC 1

--- tailwind-config.js
+++ tailwind-config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     "./src/**/*.{rs,html,css}",
     // Include Lumen Blocks components
-    `${process.env.HOME}/.cargo/git/checkouts/lumen-blocks-*/654c76c/blocks/src/**/*.rs`
+    `${process.env.HOME}/.cargo/git/checkouts/lumen-blocks-*/e73f85f/blocks/src/**/*.rs`
   ],
   theme: {
     extend: {},
